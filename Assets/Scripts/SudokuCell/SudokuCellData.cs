8e643d7eb67d21e9b002f124079675e2bfcc1a15
Lamminlun Simte <lminlunsims14@gmail.com>
2026-03-04 15:16:56 +0530

    updated cell data and UI

--- Assets/Scripts/SudokuCell/SudokuCellData.cs
+++ Assets/Scripts/SudokuCell/SudokuCellData.cs
@@ -4,9 +4,9 @@ namespace SudokuCell {
     [System.Serializable]
     public class SudokuCellData {
         [field: SerializeField]
-        public int row { get; private set; }
+        public int Row { get; private set; }
         [field: SerializeField]
-        public int col { get; private set; }
+        public int Col { get; private set; }
         public bool IsSelected { get; private set; }
     }
 }
