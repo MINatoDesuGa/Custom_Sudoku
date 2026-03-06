c0f72609a9613c640cff7697e1295cd1ba0d2007
Lamminlun Simte <lminlunsims14@gmail.com>
2026-03-06 10:23:40 +0530

    Added canvas group extensions

--- Assets/Scripts/Utility/Extensions.cs
+++ Assets/Scripts/Utility/Extensions.cs
@@ -1,4 +1,21 @@
+using UnityEngine;
+
 namespace Utility {
     public static class Extensions {
+        #region Canvas Group
+        public static void EnableGroup(this CanvasGroup canvasGroup) {
+            canvasGroup.alpha = 1.0f;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+        public static void DisableGroup(this CanvasGroup canvasGroup) {
+            canvasGroup.alpha = 0f;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+        public static bool IsEnabled(this CanvasGroup canvasGroup) { 
+            return canvasGroup.alpha == 1.0f ? true : false;    
+        }
+        #endregion
     }
 }
