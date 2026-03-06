262512f20f69a721b688f3769487dafbc97b0bdb
Lamminlun Simte <lminlunsims14@gmail.com>
2026-03-06 19:54:01 +0530

    added number input to number buttons

--- Assets/Scripts/ControlButtons/ControlButtonsInput.cs
+++ Assets/Scripts/ControlButtons/ControlButtonsInput.cs
@@ -7,7 +7,6 @@ namespace ControlButtons {
             Number_1, Number_2, Number_3, Number_4, Number_5, Number_6, Number_7, Number_8, Number_9
         }
 
-        public static event System.Action<ControlType> On_Control_Button_Click;
         public event System.Action<string> OnControlTypeInit;
 
         [SerializeField] private ControlType _controlType;
@@ -20,7 +19,7 @@ namespace ControlButtons {
             _controlButton.onClick.AddListener(OnControlButtonClick);
         }
         private void Start() {
-            OnControlTypeInit?.Invoke(GetButtonTextFromControlType());
+            OnControlTypeInit?.Invoke(GetNumberInputFromControlType().ToString());
         }
         private void OnDisable() {
             _controlButton.onClick.RemoveListener(OnControlButtonClick);
@@ -29,34 +28,15 @@ namespace ControlButtons {
 
         #region Event Listeners
         private void OnControlButtonClick() {
-            print($"Control {_controlType} click");
-            On_Control_Button_Click?.Invoke(_controlType);
+            //print($"Control {_controlType} click");
+            SudokuCell.SudokuCellController.Current_Selected_Cell_Input.OnNumberInput?.Invoke(GetNumberInputFromControlType());
         }
         #endregion
 
         #region Helper methods
-        private string GetButtonTextFromControlType() {
-            switch (_controlType) {
-                case ControlType.Number_1:
-                    return "1";
-                case ControlType.Number_2:
-                    return "2";
-                case ControlType.Number_3:
-                    return "3";
-                case ControlType.Number_4:
-                    return "4";
-                case ControlType.Number_5:
-                    return "5";
-                case ControlType.Number_6:
-                    return "6";
-                case ControlType.Number_7:
-                    return "7";
-                case ControlType.Number_8:
-                    return "8";
-                case ControlType.Number_9:
-                    return "9";
-            }
-            return null;
+        private int GetNumberInputFromControlType() {
+            string controlType = _controlType.ToString();
+            return int.Parse(controlType[^1].ToString());
         }
         #endregion
     }
