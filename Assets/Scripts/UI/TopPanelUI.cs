af4c220cef242a3285bf141b2ac6dbee1c512231
Lamminlun Simte <lminlunsims14@gmail.com>
2026-03-11 10:38:17 +0530

    added pencil mark state update in game mode button and text

--- Assets/Scripts/UI/TopPanelUI.cs
+++ Assets/Scripts/UI/TopPanelUI.cs
@@ -9,8 +9,10 @@ namespace UI {
     public class TopPanelUI : MonoBehaviour {
         private const string EDIT_MODE_BUTTON_TEXT = "EDIT"; ///TODO: picture based
         private const string SOLVE_MODE_BUTTON_TEXT = "SOLVE"; ///TODO: picture based
+        private const string PENCIL_MODE_BUTTON_TEXT = "X"; ///TODO: picture based
         private const string SOLVE_MODE_TEXT = "SOLVING";
         private const string EDIT_MODE_TEXT = "EDITING";
+        private const string PENCIL_MODE_TEXT = "MARKING";
 
         [Header("Reset Game")]
         [SerializeField] private Button _resetButton;
@@ -21,15 +23,17 @@ namespace UI {
         #region Mono
         private void Start() {
             Controller.GameStateController.UpdateGameState(GameState.Editing);
-            UpdateGameModeText(GameState.Editing);
+            //UpdateGameModeText(GameState.Editing);
         }
         private void OnEnable() {
             _resetButton.onClick.AddListener(OnResetClick);
-            _gameModeChangeButton.onClick.AddListener(OnGameModeChangButtonClick);
+            _gameModeChangeButton.onClick.AddListener(OnGameModeChangeButtonClick);
+            Controller.GameStateController.On_Game_State_Changed += UpdateGameModeText;
         }
         private void OnDisable() {
             _resetButton.onClick.RemoveListener(OnResetClick);
-            _gameModeChangeButton.onClick.RemoveListener(OnGameModeChangButtonClick);
+            _gameModeChangeButton.onClick.RemoveListener(OnGameModeChangeButtonClick);
+            Controller.GameStateController.On_Game_State_Changed -= UpdateGameModeText;
         }
         #endregion
 
@@ -37,10 +41,18 @@ namespace UI {
         private void OnResetClick() {
             Controller.GameStateController.UpdateGameState(GameState.Reset);
         }
-        private void OnGameModeChangButtonClick() {
-            GameState newGameState = Controller.GameStateController.Current_Game_State == GameState.Editing ? GameState.Solving : GameState.Editing;
+        private void OnGameModeChangeButtonClick() {
+            GameState newGameState = GameState.Solving;
+            switch (Controller.GameStateController.Current_Game_State) {
+                case GameState.Solving:
+                    newGameState = GameState.Editing;
+                    break;
+                case GameState.PencilMarking:
+                    newGameState = GameState.Solving;
+                    break;
+            }
             Controller.GameStateController.UpdateGameState(newGameState);
-            UpdateGameModeText(newGameState);
+            //UpdateGameModeText(newGameState);
         }
         #endregion
 
@@ -54,6 +66,10 @@ namespace UI {
                 case GameState.Editing:
                     _gameModeButtonText.SetText(SOLVE_MODE_BUTTON_TEXT);
                     _gameModeText.SetText(EDIT_MODE_TEXT);
+                    break;
+                case GameState.PencilMarking:
+                    _gameModeButtonText.SetText(PENCIL_MODE_BUTTON_TEXT);
+                    _gameModeText.SetText(PENCIL_MODE_TEXT);
                     break;
             }
         }
